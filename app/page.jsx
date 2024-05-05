aef54d6db8ffe0bdc3b21f128a046181ff22b55f
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-05-05 22:15:13 +0200

    file based components

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 
 const HomePage = () => {
-  return <div>homepage</div>
+  return (
+    <div>
+      <div>
+        <h1 className='text-3xl'>Welcome</h1>
+        <Link href='/properties'>Properties</Link>
+      </div>
+    </div>
+  )
 }
 
 export default HomePage
---
+++ app/properties/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+const PropertiesPage = () => {
+  return (
+    <div>
+      <div>
+        <h1 className='text-3xl'>Properties</h1>
+        <Link href='/'>Go Home</Link>
+      </div>
+    </div>
+  )
+}
+
+export default PropertiesPage
