811771297a1bf913c9554dd1d61caa50ef7be8b5
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-05-06 11:44:45 +0200

    feat: fetch single property

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,36 @@
+// client component!
 'use client'
+
+import { useEffect, useState } from 'react'
+import { useParams } from 'next/navigation'
+import { fetchProperty } from '@/utils/requests'
+// import {} from ''
+
 const PropertyPage = () => {
+  const { id } = useParams()
+
+  const [property, setProperty] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      if (!id) return
+
+      try {
+        const property = await fetchProperty(id)
+        setProperty(property)
+      } catch (error) {
+        console.error('Error fetching property', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    if (property === null) {
+      fetchPropertyData()
+    }
+  }, [id, property])
+
   return <div>PropertyPage </div>
 }
 
--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
 
+// fetch all property
 async function fetchProperties() {
   try {
     // handle the case where the API domain is not set yet
@@ -19,4 +20,23 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties }
+// fetch single property
+async function fetchProperty(id) {
+  try {
+    if (!apiDomain) {
+      return null
+    }
+    const res = await fetch(`${apiDomain}/properties/${id}`)
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+
+    return await res.json()
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
+export { fetchProperties, fetchProperty }
