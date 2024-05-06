84a6cc10dab167a6b282cbf4a49d5c82f154baeb
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-05-06 19:15:20 +0200

    feat: get user and submit form (add new record to the db) without images

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,8 +1,9 @@
 import connectDB from '@/config/database'
 import Property from '@/models/Property'
+import { getSessionUser } from '@/utils/getSessionUser'
 
 // GET /api/properties
-export const GET = async (request) => {
+export const GET = async () => {
   try {
     await connectDB()
 
@@ -20,6 +21,16 @@ export const GET = async (request) => {
 // POST /api/properties
 export const POST = async (request) => {
   try {
+    await connectDB()
+
+    const sessionUser = await getSessionUser()
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response('User ID is required', { status: 401 })
+    }
+
+    const { userId } = sessionUser
+
     const formData = await request.formData()
 
     // access all values from amenities and images
@@ -46,19 +57,27 @@ export const POST = async (request) => {
       rates: {
         weekly: formData.get('rates.weekly'),
         monthly: formData.get('rates.monthly'),
-        nightly: formData.get('rates.nightly.'),
+        nightly: formData.get('rates.nightly'),
       },
       seller_info: {
         name: formData.get('seller_info.name'),
         email: formData.get('seller_info.email'),
         phone: formData.get('seller_info.phone'),
       },
-      images,
+      owner: userId,
+      // images,
     }
 
-    return new Response(JSON.stringify({ message: 'Success' }), {
-      status: 201,
-    })
+    const newProperty = new Property(propertyData)
+    await newProperty.save()
+
+    return Response.redirect(
+      `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
+    )
+
+    // return new Response(JSON.stringify({ message: 'Success' }), {
+    //   status: 201,
+    // })
   } catch (error) {
     return new Response('Failed to add property', { status: 500 })
   }
