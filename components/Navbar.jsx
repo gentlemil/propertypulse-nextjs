c97c60e766ea6a5d9892aca63fa79779c56f3752
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-05-06 16:32:02 +0200

    feat: sign out

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -202,7 +202,12 @@ const Navbar = () => {
                     >
                       Saved Properties
                     </Link>
+
                     <button
+                      onClick={() => {
+                        setIsProfileMenuOpen(false)
+                        signOut()
+                      }}
                       className='block px-4 py-2 text-sm text-gray-700'
                       role='menuitem'
                       tabIndex='-1'
