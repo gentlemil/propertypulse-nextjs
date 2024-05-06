e0b65aa6c273aa60f8462614cd92e2e03ed8a85a
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-05-06 19:03:18 +0200

    feat: update next config file and create middleware to protect routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_URL_SECRET,
+  },
   images: {
     remotePatterns: [
       {
