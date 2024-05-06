cc179c778699c10c181dcd9e30c3e177a9d5578b
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-05-06 10:57:39 +0200

    feat: create User and Property models

---
+++ models/User.js
@@ -0,0 +1,32 @@
+import { Schema, model, models } from 'mongoose'
+
+// google authorization,
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: [true, 'Email already exists!'],
+      required: [true, 'Email is required!'],
+    },
+    username: {
+      type: String,
+      required: [true, 'Username is required!'],
+    },
+    image: {
+      type: String,
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Property', // model, not collection
+      },
+    ],
+  },
+  {
+    timestamps: true, // automatically add createdAt and updatedAt fields
+  }
+)
+
+const User = models.User || model('User', UserSchema)
+
+export default User
