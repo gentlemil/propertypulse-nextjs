6483aa9cf6fed887b8baf349b3058aea3c6c1e35
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-05-07 11:44:12 +0200

    feat: saved properties, request and client component

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -5,6 +5,31 @@ import { getSessionUser } from '@/utils/getSessionUser'
 
 export const dynamic = 'force-dynamic'
 
+// GET /api/bookmarks
+export const GET = async (request) => {
+  try {
+    await connectDB()
+
+    const { userId } = await getSessionUser()
+
+    // Find the user by their session ID
+    const user = await User.findOne({ _id: userId })
+
+    if (!user) {
+      return new Response('User not found', { status: 404 })
+    }
+
+    // Retrieve the user's bookmarks
+    const bookmarks = await Property.find({ _id: { $in: user.bookmarks } })
+
+    return new Response(JSON.stringify(bookmarks), {
+      status: 200,
+    })
+  } catch (error) {
+    return new Response('Something went wrong', { status: 500 })
+  }
+}
+
 export const POST = async (request) => {
   try {
     await connectDB()
@@ -12,7 +37,6 @@ export const POST = async (request) => {
     const { propertyId } = await request.json()
 
     const sessionUser = await getSessionUser()
-    console.log(sessionUser)
 
     if (!sessionUser || !sessionUser.userId) {
       return new Response('User ID is required', { status: 401 })
