2110c14a21b6f523af06ae6aa5bbcf729a5b7be8
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-05-08 07:20:13 +0200

    feat: featured properties, route and view

---
+++ components/FeaturedProperties.jsx
@@ -0,0 +1,28 @@
+import { fetchProperties } from '@/utils/requests'
+import FeaturedPropertyCard from '@/components/FeaturedPropertyCard'
+
+const FeaturedProperties = async () => {
+  const properties = await fetchProperties({ showFeatured: true })
+  const randomProperties = properties
+    .sort(() => Math.random() - Math.random())
+    .slice(0, 2)
+
+  return (
+    properties.length > 0 && (
+      <section className='bg-blue-50 px-4 pt-6 pb-10'>
+        <div className='container-xl lg:container m-auto'>
+          <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
+            Featured Properties
+          </h2>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+            {randomProperties.map((property, index) => (
+              <FeaturedPropertyCard property={property} key={index} />
+            ))}
+          </div>
+        </div>
+      </section>
+    )
+  )
+}
+
+export default FeaturedProperties
