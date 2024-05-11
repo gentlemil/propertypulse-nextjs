cd6e8a829bc00af7216763421254a3867f2f6235
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-05-11 12:54:18 +0200

    feat: add new property category

---
+++ app/actions/addPropertyCategoryAction.js
@@ -0,0 +1,31 @@
+'use server'
+
+import connectDB from '@/config/database'
+import Category from '@/models/Category'
+import { getSessionUser } from '@/utils/getSessionUser'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+// todo: adding new category should be available only for admin users (currently only available for logged in users)
+async function addPropertyCategory(formData) {
+  await connectDB()
+
+  const sessionUser = await getSessionUser()
+
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error('You must be logged in to add a property category')
+  }
+
+  const categoryData = {
+    name: formData.get('name'),
+  }
+
+  const newPropertyCategory = new Category(categoryData)
+  await newPropertyCategory.save()
+
+  revalidatePath('/', 'layout')
+
+  redirect(`/properties/categories`)
+}
+
+export default addPropertyCategory
--- app/properties/categories/page.jsx
+++ app/properties/categories/page.jsx
@@ -1,10 +1,14 @@
 import Link from 'next/link'
-
 import Categories from '@/components/Categories'
 import connectDB from '@/config/database'
 import Category from '@/models/Category'
 
+import { getServerSession } from 'next-auth'
+import { authOption } from '@/utils/authOptions'
+
 const PropertyCategoriesPage = async () => {
+  const data = await getServerSession(authOption)
+
   await connectDB()
 
   const categories = await Category.find({})
@@ -12,12 +16,14 @@ const PropertyCategoriesPage = async () => {
   return (
     <section className='px-4 py-2'>
       <div className='w-full flex justify-end'>
-        <Link
-          className='px-4 py-2 rounded-full bg-blue-600 text-white'
-          href='/properties/categories/add'
-        >
-          Add category
-        </Link>
+        {data?.user && (
+          <Link
+            className='px-4 py-2 rounded-full bg-blue-600 text-white'
+            href='/properties/categories/add'
+          >
+            Add category
+          </Link>
+        )}
       </div>
       <Categories categories={categories} />
     </section>
